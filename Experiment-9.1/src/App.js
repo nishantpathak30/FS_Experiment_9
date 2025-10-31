2bae7aef4769121a3199ba4b655763fd62635aab
nishantpathak30 <pathaknishant6868@gmail.com>
2025-10-31 14:20:04 +0530

    Create App.js

---
+++ Experiment-9.1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>This app is served using Nginx in a production-ready Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
